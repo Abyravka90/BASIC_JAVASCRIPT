5768a7350b89cb495110a55b306982c90dd1a22c
Abyravka90 <asepcahya.nugaha75@gmail.com>
2022-09-27 13:31:38 +0700

    pwpb_270922_basic JS Chapter 1

---
+++ commentVariable_1.js
@@ -0,0 +1,21 @@
+//single line comment
+/*
+multiple line comment
+first chapter is addition
+*/
+var x = 23.5;
+var y = 24.5;
+var addition = x+y;
+console.log(addition)
+/*
+multiple line comment
+second chapter is substraction
+*/
+/*
+multiple line comment
+third chapter is division
+*/
+/*
+multiple line comment
+fourth chapter is multiplication
+*/
